8d09e476f6eb269dffc97b165610188bdf3abe2b
Benjamin Russell <eysikal@gmail.com>
2017-10-19 12:34:22 -0600

    moved products to a separate file

--- RefurbScraper.php
+++ RefurbScraper.php
@@ -3,38 +3,15 @@ use Goutte\Client;
 
 class RefurbScraper
 {
-    public $products = [
-        '512GB_29GHz_i5' => [
-            'name' => '512GB_29GHz_i5',
-            'url' => 'https://www.apple.com/shop/product/G0QP0LL/A/refurbished-133-inch-macbook-pro-27ghz-dual-core-intel-i5-with-retina-display',
-            'email_sent' => false
-        ],
-        '1TB_29GHz_i5' => [
-            'name' => '1TB_29GHz_i5',
-            'url' => 'https://www.apple.com/shop/product/G0QP1LL/A/refurbished-133-inch-macbook-pro-27ghz-dual-core-intel-i5-with-retina-display',
-            'email_sent' => false
-        ],
-        '1TB_31GHz_i7' => [
-            'name' => '1TB_31GHz_i7',
-            'url' => 'https://www.apple.com/shop/product/G0QP2LL/A/refurbished-133-inch-macbook-pro-31ghz-dual-core-intel-i7-with-retina-display',
-            'email_sent' => false
-        ],
-        '512GB_31GHZ_i7' => [
-            'name' => '512GB_31GHZ_i7',
-            'url' => 'https://www.apple.com/shop/product/G0QP3LL/A/refurbished-133-inch-macbook-pro-27ghz-dual-core-intel-i7-with-retina-display',
-            'email_sent' => false
-        ],
-//        'in_stock_test' => [
-//            'name' => 'in_stock',
-//            'url' => 'https://www.apple.com/shop/product/FLH12LL/A/Refurbished-133-inch-Macbook-Pro-29GHz-Dual-core-Intel-Core-i5-with-Retina-Display-Space-Gray',
-//            'email_sent' => false
-//        ]
-    ];
+    public $products = [];
 
     public function __construct()
     {
         // Set timezone so Guzzle doesn't complain
         date_default_timezone_set('America/Denver');
+
+        // Pull list products from file
+        $this->products = require_once 'products.php';
 
         $this->client = new Client();
     }
